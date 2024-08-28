06c2b7bcffb1fb0bf7ed46fd99823daa5054ce47
HoangNguyenKhanhDuy <162771053+HoangNguyenKhanhDuy@users.noreply.github.com>
2024-08-28 20:53:07 +0700

    Delete BaiTap2A/BaiTap2A directory

--- BaiTap2A/BaiTap2A/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BaiTap2A.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
