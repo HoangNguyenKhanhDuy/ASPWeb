bfb616b005762732d8932168ffbb80115c4f1eeb
HoangNguyenKhanhDuy <bnguyen1456@example.com>
2024-09-14 15:07:21 +0700

    Hoan thanh bai tap

---
+++ BaiTapKiemTra01/Controllers/TaiKhoanController.cs
@@ -0,0 +1,40 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DangKy(TaiKhoanViewModel taikhoan)
+        {
+            if (taikhoan != null)
+            {
+       
+                if (!string.IsNullOrEmpty(taikhoan.Password) && taikhoan.Password.Length > 0)
+                {
+                    taikhoan.Password = taikhoan.Password + "_da_ma_hoa";
+                }
+
+                if (string.IsNullOrEmpty(taikhoan.Name))
+                {
+                    ModelState.AddModelError("HoTen", "Họ tên không được bỏ trống");
+                }
+
+                if (taikhoan.Age <= 0)
+                {
+                    ModelState.AddModelError("Tuoi", "Tuổi phải lớn hơn 0");
+                }
+                if (ModelState.IsValid)
+                {
+                    return View(taikhoan);
+                }
+            }
+            return View(taikhoan);
+        }
+    }
+}
